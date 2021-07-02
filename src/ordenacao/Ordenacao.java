c96f7975d02d826301f34925db119ad11b9e2c4a
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2021-07-02 19:43:24 -0300

    Breno 2° Entrega - Ordenacao
    adicionando classe Ordenacao

--- src/ordenacao/Ordenacao.java
+++ src/ordenacao/Ordenacao.java
@@ -1,5 +1,34 @@
 package ordenacao;
 
-public class Ordenacao {
+import java.util.List;
 
+public class Ordenacao {
+	
+	private int id;
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	protected Ordenacao(int id, String nome) {
+		setId(id);
+		setNome(nome);
+	}
+
+	public List<String> ordenar(List<String> listaOrdenacao) {
+		return listaOrdenacao;
+	}
 }
