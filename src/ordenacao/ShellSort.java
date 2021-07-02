2aeaae5cc09995f27706c83ce94f17e2a9965858
leonardo <57204667+japa2002@users.noreply.github.com>
2021-07-02 20:28:36 -0300

    1° Entrega Leonardo - Pesquisa
    adicionando método de pesquisa

--- src/ordenacao/ShellSort.java
+++ src/ordenacao/ShellSort.java
@@ -1,38 +1,23 @@
 package ordenacao;
 
-public class ShellSort {
-  
-    public SellSort(int id, String nome){  
-        super(id, nome);
-    }
+import java.util.List;
 
-    @Override
-    public List<String> ordenar(List<String> listaOrdenacao) {
-        
-        while (intervalo > 0) {
-              for (int externo = intervalo; externo < comprimento; externo++) {
-                  int valor = arr[externo];
-                  int interno = externo;
-                  while (interno > intervalo - 1 && arr[interno - intervalo] >= valor) {
-                      arr[interno] = arr[interno - intervalo];
-                      interno = interno - intervalo;
-                  }
-                  arr[interno] = valor;
-              }
-              intervalo = (intervalo - 1) / 3;
-          }
-          return arr;
-      }
-      public int CalcularIntervalo(int comprimento) {
-          int intervalo = 0;
-          while (intervalo < comprimento / 3) {
-              intervalo = intervalo * 3 + 1;
-          }
-          return intervalo;
-      }
+public class ShellSort extends Ordenacao {
 
+	public ShellSort(int id, String nome) {
+		super(id, nome);
+	}
+
+	@Override
+	public List<String> ordenar(List<String> listaOrdenacao) {
+		return null;
+	}
+
+	public int calcularIntervalo(int comprimento) {
+		int intervalo = 0;
+		while (intervalo < comprimento / 3) {
+			intervalo = intervalo * 3 + 1;
+		}
+		return intervalo;
+	}
 }
-    
-    
-    
-
--- src/pesquisa/Pesquisa.java
+++ src/pesquisa/Pesquisa.java
@@ -1,5 +1,31 @@
 package pesquisa;
 
-public class Pesquisa {
+import java.util.List;
 
+public class Pesquisa {
+	
+	private int idPesquisa;
+    private String nome;
+
+    public int getIdPesquisa() {
+        return idPesquisa;
+    }
+    public void setIdPesquisa(int idPesquisa) {
+        this.idPesquisa = idPesquisa;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int pesquisar(List<String> lista, String conteudo){
+        return 0;
+    }
+
+    protected Pesquisa(int idPesquisa, String nome) {
+        this.idPesquisa = idPesquisa;
+        this.nome = nome;
+    }
 }
