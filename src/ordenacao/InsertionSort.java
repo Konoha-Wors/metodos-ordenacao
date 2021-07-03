dc993550779320b227003a7ca5b5b32b97695249
Pedro dos Santos <71856307+pedrozaou1@users.noreply.github.com>
2021-07-02 21:21:56 -0300

    2° Entrega - Metodo Insertion
    método de ordenação

--- src/ordenacao/InsertionSort.java
+++ src/ordenacao/InsertionSort.java
@@ -1,5 +1,49 @@
 package ordenacao;
 
-public class InsertionSort {
+import java.util.List;
+
+public class InsertionSort extends Ordenacao {
+
+	public InsertionSort(int id, String nome) {
+		super(id, nome);
+	}
+
+	@Override
+	public List<String> ordenar(List<String> listaOrdenacao) {
+	
+		int[] numeros = new int[listaOrdenacao.size()];
+		boolean entrou = true;
+		
+		for (int i = 0; i < listaOrdenacao.size(); i++) {
+			numeros[i] = Integer.parseInt(listaOrdenacao.get(i));
+		}
+		
+		for (int i = 1; i < numeros.length; i++) {
+
+			int indice = i;
+			entrou = true;
+
+			while (entrou) {
+				entrou = false;
+
+				if (indice > 0) {
+					if (numeros[indice] < numeros[indice - 1]) {
+						int numTemp = numeros[indice];
+						numeros[indice] = numeros[indice - 1];
+						numeros[indice-1] = numTemp;
+						entrou = true;
+					}
+				}
+				indice--;
+			}
+		}
+		
+		listaOrdenacao.clear();
+		for (int i = 0; i < numeros.length; i++) {
+			listaOrdenacao.add(String.valueOf(numeros[i]));
+		}
+		
+		return listaOrdenacao;
+	}
 
 }
--- src/tela/Main.java
+++ src/tela/Main.java
@@ -1,5 +1,20 @@
 package tela;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ordenacao.InsertionSort;
+
 public class Main {
+	
+	public static void main(String[] args) {
+		List<String> lista = new ArrayList<>();
+		lista.add("9");
+		lista.add("1");
+		lista.add("7");
+		InsertionSort insertion = new InsertionSort(0, null);
+		List<String> lista2 = insertion.ordenar(lista);
+		System.out.println(lista2);
+	}
 
 }
