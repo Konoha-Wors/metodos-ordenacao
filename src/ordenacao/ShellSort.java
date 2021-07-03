96a353b0eab2b9d5c795d5c39682aa48d4a0466a
MrJohn2001 <74744796+MrJohn2001@users.noreply.github.com>
2021-07-02 22:56:06 -0300

    Entrega João Pedro Menezes - Ordenação Shell Sort

--- src/ordenacao/ShellSort.java
+++ src/ordenacao/ShellSort.java
@@ -10,6 +10,38 @@ public class ShellSort extends Ordenacao {
 
 	@Override
 	public List<String> ordenar(List<String> listaOrdenacao) {
-		return super.ordenar(listaOrdenacao);
+		int intervalo = calcularIntervalo(listaOrdenacao.size());
+
+		int[] arr = new int[listaOrdenacao.size()];
+		for (int i = 0; i < listaOrdenacao.size(); i++) {
+		    arr[i] = Integer.parseInt(listaOrdenacao.get(i));
+		}
+
+		while (intervalo > 0) {
+		    for (int externo = intervalo; externo < arr.length; externo++) {
+			int valor = arr[externo];
+			int interno = externo;
+			while (interno > intervalo - 1 && arr[interno - intervalo] >= valor) {
+			    arr[interno] = arr[interno - intervalo];
+			    interno = interno - intervalo;
+			}
+			arr[interno] = valor;
+		    }
+		    intervalo = (intervalo - 1) / 3;
+		}
+
+		listaOrdenacao.clear();
+		for (int i = 0; i < arr.length; i++) {
+		    listaOrdenacao.add(String.valueOf(arr[i]));
+		}
+
+		return listaOrdenacao;
 	}
+	private int calcularIntervalo(int comprimento) {
+		int intervalo = 0;
+		while (intervalo < comprimento / 3) {
+		    intervalo = intervalo * 3 + 1;
+		}
+        return intervalo;
+    }
 }
