416092ca24de97a0a24684eb66b309aad76c50b2
rafaeltschoecke <rafaeltschoecke1@gmail.com>
2021-07-02 22:10:01 -0300

    3° Entrega - Rafael Ordenacao
    Selection Sort

--- src/ordenacao/SelectionSort.java
+++ src/ordenacao/SelectionSort.java
@@ -1,5 +1,40 @@
 package ordenacao;
 
-public class SelectionSort {
+import java.util.List;
 
+public class SelectionSort extends Ordenacao{
+
+	protected SelectionSort(int id, String nome) {
+		super(id, nome);
+	}
+
+	@Override
+	public List<String> ordenar(List<String> listaOrdenacao) {
+
+		int[] numeros = new int[listaOrdenacao.size()];
+		for (int i = 0; i < listaOrdenacao.size(); i++) {
+            numeros[i] = Integer.parseInt(listaOrdenacao.get(i));
+        }
+
+		for (int i = 0; i < numeros.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < numeros.length; j++) {
+				if (numeros[min] > numeros[j]) {
+					min = j;
+				}
+			}
+
+			int temp = numeros[i];
+			numeros[i] = numeros[min];
+			numeros[min] = temp;
+		}
+		
+		listaOrdenacao.clear();
+        for (int i = 0; i < numeros.length; i++) {
+            listaOrdenacao.add(String.valueOf(numeros[i]));
+        }
+        
+		return listaOrdenacao;
+	}
+	
 }
